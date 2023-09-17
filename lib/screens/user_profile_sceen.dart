75a478a086f7e718c7e94a68c84ba06aa3494e7b
Ehsan973 <81531659+Ehsan973@users.noreply.github.com>
2023-09-17 13:36:31 +0330

    add posts to profile screen

--- lib/screens/user_profile_sceen.dart
+++ lib/screens/user_profile_sceen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class UserProfileScreen extends StatelessWidget {
   const UserProfileScreen({super.key});
@@ -9,8 +10,121 @@ class UserProfileScreen extends StatelessWidget {
       backgroundColor: Color(0xff1C1F2E),
       body: SafeArea(
         child: Center(
-          child: _getHeaderProfile(),
+          child: DefaultTabController(
+            length: 2,
+            child: NestedScrollView(
+              headerSliverBuilder: (context, innerBoxIsScrolled) {
+                return [
+                  SliverAppBar(
+                    // pinned: true,
+                    toolbarHeight: 60,
+                    actions: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 18.0, right: 18),
+                        child: Icon(Icons.menu),
+                      ),
+                    ],
+                    bottom: PreferredSize(
+                      preferredSize: Size.fromHeight(10),
+                      child: Container(
+                        height: 14,
+                        decoration: BoxDecoration(
+                          color: Color(0xff1C1F2E),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(15),
+                            topRight: Radius.circular(15),
+                          ),
+                        ),
+                      ),
+                    ),
+                    backgroundColor: Color(0xff1C1F2E),
+                    expandedHeight: 170,
+                    flexibleSpace: FlexibleSpaceBar(
+                      background: Image.asset(
+                        'assets/images/item1.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  SliverToBoxAdapter(
+                    child: _getHeaderProfile(),
+                  ),
+                  SliverPersistentHeader(
+                    pinned: true,
+                    // floating: true,
+                    delegate: TabBarViewDelegate(
+                      TabBar(
+                        indicatorColor: Color(0xffF35383),
+                        tabs: [
+                          Tab(
+                            icon:
+                                Image.asset('assets/images/icon_tab_posts.png'),
+                          ),
+                          Tab(
+                            icon: Image.asset(
+                                'assets/images/icon_tab_bookmark.png'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ];
+              },
+              body: TabBarView(
+                children: [
+                  CustomScrollView(
+                    slivers: [
+                      SliverPadding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                        sliver: _getPostGrid(),
+                      ),
+                    ],
+                  ),
+                  CustomScrollView(
+                    slivers: [
+                      SliverPadding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                        sliver: _getPostGrid(),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
+      ),
+    );
+  }
+
+  SliverGrid _getPostGrid() {
+    return SliverGrid(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.asset(
+                'assets/images/item${index % 10}.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          );
+        },
+      ),
+      gridDelegate: SliverQuiltedGridDelegate(
+        crossAxisCount: 3,
+        pattern: [
+          QuiltedGridTile(1, 1),
+          QuiltedGridTile(2, 2),
+          QuiltedGridTile(1, 1),
+          QuiltedGridTile(1, 1),
+          QuiltedGridTile(1, 1),
+          QuiltedGridTile(1, 1),
+        ],
       ),
     );
   }
@@ -78,5 +192,33 @@ class UserProfileScreen extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class TabBarViewDelegate extends SliverPersistentHeaderDelegate {
+  final TabBar _tabBar;
+
+  TabBarViewDelegate(this._tabBar);
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Color(0xff1C1F2E),
+      child: _tabBar,
+    );
+  }
+
+  @override
+  // TODO: implement maxExtent
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  // TODO: implement minExtent
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
